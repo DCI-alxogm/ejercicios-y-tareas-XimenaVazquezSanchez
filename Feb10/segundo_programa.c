e8a44f3cc723a11d15740fedae14112e13379591
Estudiantes DCI <x.vazquezsanchez@ugto.mx>
2025-02-10 11:41:58 -0600

    Segundo programa evaluación del día 10/02/2025

---
+++ Feb10/segundo_programa.c
@@ -0,0 +1,12 @@
+/* Creado el 10 de Febrero de 2025 por ximena vazquez */
+
+#include<stdio.h>
+
+int main() 
+{   
+     
+    printf ("Bienvenida Ximena \n");
+    printf ("Este es el segundo programa creado para este curso de programación básica \n");
+}
+
+/*Fin de el programa*/

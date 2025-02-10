a4309772548469bd94c0fd89054a1a3955c08087
Estudiantes DCI <x.vazquezsanchez@ugto.mx>
2025-02-10 11:26:31 -0600

    Actualización del dia 10/02/2025

---
+++ Feb10/primer_programa.c
@@ -0,0 +1,12 @@
+/* Creado el 10 de Febrero de 2025 por ximena vazquez */
+
+#include<stdio.h>
+
+int main() 
+{   
+     
+    printf ("Hola Ximena \n");
+    printf ("Este es el primer programa del curso\n");
+}
+
+/*Termine de escribir el programa*/

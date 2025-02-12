baeb50fa890aa644d181d187ea26633f0c68b7a6
Estudiantes DCI <x.vazquezsanchez@ugto.mx>
2025-02-12 12:03:26 -0600

    Segundo Programa

---
+++ Feb12/Mi_primerprograma_PB2025.c
@@ -0,0 +1,22 @@
+/*
+Agosto 12, 2025
+Ximena Vazquez Sanchez
+Primer programa, mostrando estructura, definición de variables, imprimir variables
+*/
+
+#include <stdlib.h> // libreria estanda C.
+#include <stdio.h> // libreria para interaccion con la pantalla.
+
+
+int main ( ) {
+
+	char nombre[10];
+	int edad;
+	float temperatura;
+	//Cuerpo del programa;
+	printf ("¿Cual es tu nombre? \n");
+	scanf("%s", nombre);
+	printf ("Hola, %s este es el primer programa del curso PB2025\n", nombre); // el caracter \n significa un salto de linea
+
+
+}

b9e966c72dcf3a088802feb156f6e86bcb110b71
Estudiantes DCI <x.vazquezsanchez@ugto.mx>
2025-02-17 11:45:57 -0600

    Programa de jerarquía de números enteros

---
+++ Feb17/operaciones_int.c
@@ -0,0 +1,27 @@
+/*
+Febrero 17, 2021
+Ximena Vázquez
+Programa de Jerarquía de operaciones con enteros 
+*/
+
+
+#include <stdio.h>
+
+int main (){
+          int a,b,c,d;
+          int e1,e2,e3,e4;
+
+          a=3;
+          b=4;
+          c=5;
+          d=7;
+          e1=(a+b)*c/d;
+          e2=((a+b)*c)/d;
+          e3=(a+b)+(c/d);
+          e4=a+b*c/d;
+         printf("e=(a+b)*c/d%d\n",e1);
+         printf("e=((a+b)*c)/d%d\n",e2);
+         printf("e=(a+b)+(c/d)%d\n",e3);
+         printf("e=a+b*c/d%d\n",e4);
+      
+}

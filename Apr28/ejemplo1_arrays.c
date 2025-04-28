2a520847c21bc5c6dc04700a273302cf621483b2
Estudiantes DCI <x.vazquezsanchez@ugto.mx>
2025-04-28 11:43:28 -0600

    Ejemplo 1. Arreglos-Inicialización a cero.

---
+++ Apr28/ejemplo1_arrays.c
@@ -0,0 +1,28 @@
+/*
+Nombre: Ximena Vázquez Sánchez 
+Fecha: 28 de abril 2025
+Ejemplo 1: Inicialización a cero (Arreglos)
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+ 
+float array_2d[1][2]=0.0;
+
+ for(i=0;i<n_filas;i++){
+   for(j=0;j<m_cols;j++){
+           scanf("%f",&array_2d[i][j]);
+      }
+  }
+
+for (i=0;i<n_filas;i++){
+    for (j=0; j<m_cols; j++){
+         printf("%f, t", &array_2d[i][j]);
+       }
+      printf("\n");
+    }
+
+  return 0;
+
+   }
